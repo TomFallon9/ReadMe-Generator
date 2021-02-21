0063e536216815d45ca66b77af45749f48063a5a
Tom Fallon <tomfallon9@gmail.com>
2021-02-21 13:48:27 -0500

    fixed links to the table of contents in readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,46 +1,49 @@
 function generateMarkdown(title, licenseBadge, description, installation, usage, license, contribution, testing, username, email, avatarURL) {
-  const readmeText = `
+  const readmeText =
+  `
   # Title: ${title}
+ 
   ${licenseBadge}
+
   
   ## Description
   ${description}
   
   ## Table of Contents
   
-  1. [ Installation ](#installation)
-  2. [ Usage ](#usage)
-  3. [ License ](#license)
-  4. [ Contributing ](#contributing)
-  5. [ Tests ](#tests)
-  6. [ Questions ](#questions)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
       
   <a name="installation"></a>
   ## 1. Installation
   
   ${installation}
   <a name="usage"></a>
-  ## 2. Usage tips
+  ## Usage tips
   
   ${usage}
   
   <a name="license"></a>
-  ## 3. License
+  ## License
   
   ${license}
   
   <a name="contributing"></a>
-  ## 4. Contributing
+  ## Contributing
   
   ${contribution}
   
   <a name="Tests"></a>
-  ## 5. Tests
+  ## Tests
   
   ${testing}
   
   <a name="Questions"></a>
-  ## 6. Questions
+  ## Questions
   
   Any Questions? Reach me here! ${email}. See more of my work and reach out if you would like to collaborate! ${username}
   <img src="${avatarURL}" alt="profile pic for username">
